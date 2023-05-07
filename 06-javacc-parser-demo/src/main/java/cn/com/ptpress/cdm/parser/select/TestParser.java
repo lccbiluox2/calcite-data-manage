6ed0c93e3dd7f28638d5b72a52d0c95ee953b12b
gaoliang <gaoliang@stargraph.cn>
2023-05-07 12:52:50 +0800

    [master]

---
+++ 06-javacc-parser-demo/src/main/java/cn/com/ptpress/cdm/parser/select/TestParser.java
@@ -0,0 +1,17 @@
+package cn.com.ptpress.cdm.parser.select;
+
+import java.text.ParseException;
+
+public class TestParser {
+
+    public static void main(String[] args) throws ParseException {
+        parseSelect("select 1+1");
+        parseSelect("select 1+1+1");
+        parseSelect("select 1 + 3 - 5");
+    }
+
+    private static void parseSelect(String sql) throws ParseException {
+        final SimpleSelectParser parser = new SimpleSelectParser(sql);
+        parser.parse();
+    }
+}
